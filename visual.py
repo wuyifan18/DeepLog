93ca6fa34739050729c4af91357d7291f2f01108
wuyifan18 <wuyifan018@gmail.com>
2019-06-05 15:59:40 +0800

    modify visual.py

--- visual.py
+++ visual.py
@@ -10,11 +10,8 @@ index = np.arange(3)
 bar_width = 0.2
 opacity = 0.4
 
-rects1 = ax.bar(index, PCA, bar_width,
-                alpha=opacity, color='b', label='PCA')
-
-rects2 = ax.bar(index + bar_width, LSTM, bar_width,
-                alpha=opacity, color='r', label='LSTM')
+rects1 = ax.bar(index, PCA, bar_width, alpha=opacity, color='b', label='PCA')
+rects2 = ax.bar(index + bar_width, LSTM, bar_width, alpha=opacity, color='r', label='LSTM')
 
 ax.set_xlabel('Measure')
 ax.set_ylabel('Scores')
@@ -25,15 +22,18 @@ ax.legend()
 plt.show()
 
 x = [8, 9, 10, 11]
-precision = [0.84313, 0.85652, 0.86877, 0.88274]
-recall = [0.92554, 0.90492, 0.87509, 0.84356]
-f1 = [0.88241, 0.88006, 0.87192, 0.86271]
-l1 = plt.plot(x, precision, ':rx')
-l2 = plt.plot(x, recall, ':b+')
-l3 = plt.plot(x, f1, ':k^')
-plt.xlabel('Top candidate')
+FP = [605, 588, 495, 860]
+FN = [465, 333, 108, 237]
+TP = [4123 - FN[i] for i in range(4)]
+P = [TP[i] / (TP[i] + FP[i]) for i in range(4)]
+R = [TP[i] / (TP[i] + FN[i]) for i in range(4)]
+F1 = [2 * P[i] * R[i] / (P[i] + R[i]) for i in range(4)]
+l1 = plt.plot(x, P, ':rx')
+l2 = plt.plot(x, R, ':b+')
+l3 = plt.plot(x, F1, ':k^')
+plt.xlabel('window_size')
 plt.ylabel('scores')
 plt.legend((l1[0], l2[0], l3[0]), ('Precision', 'Recall', 'F1-score'))
-plt.xticks([8, 9, 10, 11])
+plt.xticks(x)
 plt.ylim((0.5, 1))
 plt.show()
